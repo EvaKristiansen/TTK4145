a08326ba776660e1ce1a5a10c07f8cb147fc711c
Guest <guest-IWmU8X@itk-d650>
2017-01-13 12:09:43 +0100

    Needs more fixing,tried to fix race conditions with buffer channels

--- exercise2/threadrace_fix.go
+++ exercise2/threadrace_fix.go
@@ -7,25 +7,49 @@ import(
 	"time"
 )
 
-var i = 0;
+var counterbuff = make(chan int, 1);
+var busybuff = make(chan bool, 1);
 
 func thread_1(){
 	for j := 0; j < 1000000; j++{
+		check := false
+		while (check == false){
+			check = <- busybuff
+		}
+		
+		busybuff <- true
+		i := <- counterbuff
+		
 		i++;
+		counterbuff <- i
+		busybuff <- false
 	}
 }
 func thread_2(){
-	for k := 0; k < 1000000; k++{
+	for j := 0; j < 1000000; j++{
+		check := false
+		while (check == false){
+			check = <- busybuff
+		}
+
+		busybuff <- true
+		i := <- counterbuff
 		i--;
+		counterbuff <- i
+		busybuff <- false
 	}
 }
 
 func main(){
-	runtime.GOMAXPROCS(2)
+	i := 0
+	counterbuff <- i
+	busybuff <- false 
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	go thread_1()
 	go thread_2()
 	
 	time.Sleep(100*time.Millisecond)
+	i = <- counterbuff	
 	Println(i)
 }
 
