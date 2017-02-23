ba2e8f47373ffc4a595de7d30be7377225dae841
Eva Kristiansen <eva.kris111@gmail.com>
2017-02-23 16:23:30 +0100

    Update queue.erl

--- elevator/queue.erl
+++ elevator/queue.erl
@@ -1,48 +1,28 @@
 
--module (queue).
--export( [init/0, add_to_queue/3, addToInnerQueue/2, mergeFromQueue/2, 
-	getInnerQueue/2, getNextOrder/1, deleteFromQueue/2, isOrder/2]).
 
-% Order i a tuple of the form {inner/outer, floor, direction}
+-module (queue).
+-export( [init/0, add_to_queue/3, addToInnerQueue/2, mergeFromQueue/2, getInnerQueue/2, getNextOrder/1, deleteFromQueue/2, isOrder/2]).
+
+% Order in a tuple of the form {floor, direction}
+- record(order,{floor,direction}).
 
 - define(IN_KEY = 13).
 - define(OUT_KEY = 42).
 - define(QUEUE_PID = queue).
-- define(INNER = 0).
-- define(OUTER = 1).
-
-
-% makes a file type to bag, witch corresponds to map. Må vel være dynamisk og tilpasse til antall noder?
+- define(ELEVATORS = elevators)
 
 init() ->
-	MyQueue_inner = ordsets:new(),
-	MyQueue_outer = ordsets:new(),
-	MyDict_ini = dict:append(IN_KEY, MyQueue_inner, dict:new()),
-	MyDict = dict:append(OUT_KEY, MyQueue_outer, MyDict_ini),
-	
-	
-	
-	QueueID1_inner = ordsets:new(),
-	QueueID1_outer = ordsets:new(),
-	DictID1_ini = dict:append(IN_KEY, QueueID1_inner, dict:new()),
-	DictID1 = dict:append(OUT_KEY, QueueID1_outer, QueueID1_ini),
-	
-	
-	QueueID2_inner = ordsets:new(),
-	QueueID2_outer = ordsets:new(),
-	DictID2_ini = dict:append(IN_KEY, QueueID2_inner, dict:new()),
-	DictID2 = dict:append(OUT_KEY, QueueID2_outer, QueueID2_ini),
-	
-
-	register(?QUEUE_PID, spawn(?MODULE, queue_storage_loop, [MyDict, DictID1, DictID2]).
+	Memberlist = get_members(),
+	Queues = init_storage(Queues, Memberlist),
+	register(?QUEUE_PID, spawn(?MODULE, queue_storage_loop, Queues).
 
 
-init_from_stop ->
+init_from_stop -> %?
 	% Modulen skal spørre andre tilkoplede heiser om deres ytre og indre kø.
 	% Bruker "insert" egenskapen i queue_storage som skal overskrive eventuelle data som er på "dict -> key"
 	
 
-queue_storage_loop(MyQueue, Queue1, Queue2) ->
+queue_storage_loop(Queues) ->
 	receive
 		{get_set, {Pid, {Id, Key, _Floor, _Direction}}} ->
 			Pid ! dict:find(Key, Id),
@@ -95,3 +75,16 @@ deleteFromQueue(ElevatorID, CurrentFloor) ->
 	?QUEUE_PID ! {remove, {ID, OUT_KEY, CurrentFloor, Direction}}
 
 isOrder(Floor, ButtonType) ->
+
+init_storage(Queues,Memberlist)->
+	case Memberlist of 
+		[Member | Rest] ->	
+			In_name = atom_to_list(member) + "_inner",
+			Out_name = atom_to_list(member) + "_outer",
+
+			Temp_dict = dict:append(In_name, ordsets:new(), Queues),
+			New_Queues = dict:append(Out_name, ordsets:new(), Temp_dict),
+			init_storage(New_Queues,Rest);
+		[] ->
+			Queues;
+	end.
